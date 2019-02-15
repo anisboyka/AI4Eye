c55b55900a313091556646c87e555e2f938846a9
Bournadet Alex <alex.bournadet@gmail.com>
2019-02-15 16:19:00 +0100

    Import Render template + connection page

--- Flask-tuto.py
+++ Flask-tuto.py
@@ -3,13 +3,13 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-    return "Hello !"
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
